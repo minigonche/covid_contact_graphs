58ef017d3c7b1dc57f3a17375b88e2b040ca08e4
Jaime Cascante <jaimec@whaleandjaguar.co>
2020-11-06 00:31:09 -0500

    typo

--- figure_generation/constants.py
+++ figure_generation/constants.py
@@ -1,7 +1,6 @@
 # Constants
 
-reports_folder_location = "/home/minigonche/Dropbox/Projects/covid_fb_pipeline/data_repo/report"
-reports_folder_location = "/home/minigonche/NS"
+reports_folder_location = "/home/jupyter/Dropbox/covid_fb/report"
 figure_folder_name = "report_network_figures"
 ident = '      '
 date_format = "%Y-%m-%d"
