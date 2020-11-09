cbe90dab9f1e2dde8d564db7508c435854066e24
ChaosDonkey06 <je.cascante10@uniandes.edu.co>
2020-11-09 14:57:59 -0500

    contact graphs

--- figure_generation/constants.py
+++ figure_generation/constants.py
@@ -4,3 +4,24 @@ reports_folder_location = "/home/jupyter/Dropbox/covid_fb/report"
 figure_folder_name = "report_network_figures"
 ident = '      '
 date_format = "%Y-%m-%d"
+
+
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(1,1, figsize=(15.5, 7))
+ax.plot(df_graph_attr_treatment.index.values,
+        df_graph_attr_treatment.num_contacts_change_percentage.values, 
+        color='k',
+        label='%')
+
+ax.plot(df_graph_attr_treatment.index.values,
+        df_graph_attr_treatment.num_contacts_change_percentage_weighted.values, 
+        color='r',
+        label=r'$\%$ graph_size weighted')
+
+ax.set_xlabel('Date')
+ax.set_ylabel('Cambio porcentual en los contactos [%]')
+#ax.set_yscale('log')
+
+ax.legend()
+plt.show()
--- update_attributes/graphs_attributes/graph_percentage_mean_deg_change.py
+++ update_attributes/graphs_attributes/graph_percentage_mean_deg_change.py
@@ -17,7 +17,6 @@ property_values = {}
 # Attribute name
 property_values['attribute_name'] = 'percetange_contact_change'
 
-
 class GraphPercentageContactChange(GenericGraphAttributeWithCases):
     '''
     Script that computes the average distance to infected
@@ -77,72 +76,52 @@ class GraphPercentageContactChange(GenericGraphAttributeWithCases):
         # Extracts the value
         value = df['value'].values[0]
         
-        if pd.isna(value):
-            print('             ' + f'No distance to infected found for {graph_id} on {end_date_string}')
-            return(pd.DataFrame({'value':None, 'attribute_name':[self.attribute_name] }))
-        df_response = pd.DataFrame({'value':[value], 'attribute_name':[self.attribute_name] })
+        attributes_graph = ['number_of_cases_accumulated',
+                'largest_eigenvalue_unweighted',
+                'graph_size',
+                'average_distance_to_infected',
+                'graph_num_edges',
+                'powerlaw_degree_ks_statistic',
+                'powerlaw_degree_is_dist',
+                'number_of_contacts',
+                'largest_eigenvalue_weighted',
+                'graph_transitivity',
+                'pagerank_gini_index',
+                'eigenvector_gini_index',
+                'personalized_pagerank_gini_index',
+                'powerlaw_degree_p_value',
+                'powerlaw_degree_alpha']
+
+        df_city = df_graph_attributes.copy()
+        df_city = df_city.query("type=='city'")
+        df_city = df_city.set_index('location_id','attribute_value')
+        df = []
+        for location_id in set(df_city.index.values) :
+            type_id = df_city.loc[location_id]['type'].unique()[0]
+            df_location_id = df_city.loc[location_id].set_index('attribute_name')
+
+            df_location_id         = df_location_id.copy().loc[['number_of_contacts', 'graph_num_edges', 'graph_size']] #, 'largest_eigenvalue_unweighted', 'largest_eigenvalue_weighted']]
+            df_location_id['date'] = pd.to_datetime(df_location_id['date'])
+            df_location_id         = df_location_id.sort_values(by='date')
+
+            df_graph_attr_treatment = df_location_id.pivot_table(values='attribute_value', index='date', columns='attribute_name', aggfunc='first')
+            num_contacts_baseline   = df_graph_attr_treatment['number_of_contacts'].iloc[:3].mean()
+            graph_size_baseline     = df_graph_attr_treatment['graph_size'].iloc[:3].mean()
+            df_graph_attr_treatment[ 'num_contacts_change_percentage' ]          = df_graph_attr_treatment.apply(lambda x: (x['number_of_contacts']-num_contacts_baseline)/num_contacts_baseline*100, axis=1)
+            df_graph_attr_treatment[ 'num_contacts_change_percentage_weighted' ] = df_graph_attr_treatment.apply(lambda x: (x['number_of_contacts']-num_contacts_baseline)/num_contacts_baseline*x['graph_size']/graph_size_baseline*100, axis=1 ) 
+            df_graph_attr_treatment = df_graph_attr_treatment.dropna()
+            df_graph_attr_treatment['location_id'] = location_id
+            df_graph_attr_treatment['type'] = type_id
+            df.append(df_graph_attr_treatment)
+
+        df_response = pd.concat(df)
         
         return(df_response)
 
 
 ####################################################################################################################
-df_graph_attributes = pd.read_csv('/Users/chaosdonkey06/Dropbox/covid_graphs/graph_attributes/graph_attributes.csv')
 
-attributes_graph = ['number_of_cases_accumulated',
-        'largest_eigenvalue_unweighted',
-        'graph_size',
-        'average_distance_to_infected',
-        'graph_num_edges',
-        'powerlaw_degree_ks_statistic',
-        'powerlaw_degree_is_dist',
-        'number_of_contacts',
-        'largest_eigenvalue_weighted',
-        'graph_transitivity',
-        'pagerank_gini_index',
-        'eigenvector_gini_index',
-        'personalized_pagerank_gini_index',
-        'powerlaw_degree_p_value',
-        'powerlaw_degree_alpha']
+# df_graph_attributes = pd.read_csv('/Users/chaosdonkey06/Dropbox/covid_graphs/graph_attributes/graph_attributes.csv')
 
-df_city = df_graph_attributes.copy()
-df_city = df_city.query("type=='city'")
-df_city = df_city.set_index('location_id','attribute_value')
-df = []
-for location_id in set(df_city.index.values) :
-    type_id = df_city.loc[location_id]['type'].unique()[0]
-    df_location_id = df_city.loc[location_id].set_index('attribute_name')
 
-    df_location_id         = df_location_id.copy().loc[['number_of_contacts', 'graph_num_edges', 'graph_size']] #, 'largest_eigenvalue_unweighted', 'largest_eigenvalue_weighted']]
-    df_location_id['date'] = pd.to_datetime(df_location_id['date'])
-    df_location_id         = df_location_id.sort_values(by='date')
 
-    df_graph_attr_treatment = df_location_id.pivot_table(values='attribute_value', index='date', columns='attribute_name', aggfunc='first')
-    num_contacts_baseline   = df_graph_attr_treatment['number_of_contacts'].iloc[:3].mean()
-    graph_size_baseline     = df_graph_attr_treatment['graph_size'].iloc[:3].mean()
-    df_graph_attr_treatment[ 'num_contacts_change_percentage' ]          = df_graph_attr_treatment.apply(lambda x: (x['number_of_contacts']-num_contacts_baseline)/num_contacts_baseline*100, axis=1)
-    df_graph_attr_treatment[ 'num_contacts_change_percentage_weighted' ] = df_graph_attr_treatment.apply(lambda x: (x['number_of_contacts']-num_contacts_baseline)/num_contacts_baseline*x['graph_size']/graph_size_baseline*100, axis=1 ) 
-    df_graph_attr_treatment = df_graph_attr_treatment.dropna()
-    df_graph_attr_treatment['location_id'] = location_id
-    df_graph_attr_treatment['type'] = type_id
-    df.append(df_graph_attr_treatment)
-
-df_response = pd.concat(df)
-
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots(1,1, figsize=(15.5, 7))
-ax.plot(df_graph_attr_treatment.index.values,
-        df_graph_attr_treatment.num_contacts_change_percentage.values, 
-        color='k',
-        label='%')
-
-ax.plot(df_graph_attr_treatment.index.values,
-        df_graph_attr_treatment.num_contacts_change_percentage_weighted.values, 
-        color='r',
-        label=r'$\%$ graph_size weighted')
-
-ax.set_xlabel('Date')
-ax.set_ylabel('Cambio porcentual en los contactos [%]')
-#ax.set_yscale('log')
-
-ax.legend()
-plt.show()
